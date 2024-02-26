c38885cf2aacfa5896eda7e86d6a39bc5dfd56bf
Kate <1234567@gmail.com>
2024-02-26 12:15:01 +0300

    Initial commit

---
+++ src/main/java/ru/ifellow/baranova_1ifellow/Baranova1IfellowApplication.java
@@ -0,0 +1,13 @@
+package ru.ifellow.baranova_1ifellow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Baranova1IfellowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Baranova1IfellowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/ifellow/baranova_1ifellow/Baranova1IfellowApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ifellow.baranova_1ifellow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Baranova1IfellowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
